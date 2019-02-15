9f2678b34a0d2348bc3d20cc0bc26f61bceb88a7
TFotakis <fotakistzanis@gmail.com>
2019-02-16 01:59:19 +0200

    Removed shop app

--- shop/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from django.utils.translation import gettext
-
-
-def homeView(request):
-	context = {
-		'title': gettext('Shop') + ' - VegItNow',
-	}
-	return render(request=request, template_name='shop/home.html', context=context)
