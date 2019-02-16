cf11bcafc9052f4e1210fbe56b8256eddbab3434
TFotakis <fotakistzanis@gmail.com>
2019-02-16 02:05:12 +0200

    Added database administration link on Admin.js router

--- frontend/src/router/Admin.js
+++ frontend/src/router/Admin.js
@@ -3,32 +3,44 @@ import Administration from '../components/Admin/Administration';
 import AdminArticleList from '../components/Admin/AdminArticleList';
 import AdminRecipeList from '../components/Admin/AdminRecipeList';
 import AdminIngredientList from '../components/Admin/AdminIngredientList';
+import MainAdministration from '../components/Admin/MainAdministration';
 
-const basePath = '/s6AptmegHaGM3Ry5vdlr/admin/';
+const basePath = '/s6AptmegHaGM3Ry5vdlr/';
 
 export default [
 	{
 		path: basePath,
+		name: 'MainAdministration',
+		component: MainAdministration
+	},
+	{
+		path: basePath + 'database/',
+		name: 'DatabaseAdministration',
+		pathToRegexpOptions: {strict: true}
+		// component: MainAdministration
+	},
+	{
+		path: basePath + 'admin/',
 		name: 'Administration',
 		component: Administration
 	},
 	{
-		path: basePath + 'createArticle/',
+		path: basePath + 'admin/createArticle/',
 		name: 'CreateArticle',
 		component: CreateArticle
 	},
 	{
-		path: basePath + 'articles/',
+		path: basePath + 'admin/articles/',
 		name: 'AdminArticleList',
 		component: AdminArticleList
 	},
 	{
-		path: basePath + 'recipes/',
+		path: basePath + 'admin/recipes/',
 		name: 'AdminRecipeList',
 		component: AdminRecipeList
 	},
 	{
-		path: basePath + 'ingredients/',
+		path: basePath + 'admin/ingredients/',
 		name: 'AdminIngredientList',
 		component: AdminIngredientList
 	}
