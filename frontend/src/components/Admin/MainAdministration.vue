446ed11f088afb866e0de252cdff35bc989c56a1
TFotakis <fotakistzanis@gmail.com>
2019-02-16 02:01:34 +0200

    Created MainAdministration component

---
+++ frontend/src/components/Admin/MainAdministration.vue
@@ -0,0 +1,19 @@
+<template>
+	<div id="Administration" class="container mb-5 h-100">
+		<div class="navbar-placeholder"></div>
+		<h1 class="text-center">Main Administration</h1>
+		<router-link :to="{ name: 'Administration' }" class="nav-link font-weight-bold">{{ $t('Administration') }}</router-link>
+		<router-link :to="{ name: 'DatabaseAdministration' }" class="nav-link font-weight-bold">{{ $t('Database') }}</router-link>
+		<router-view></router-view>
+	</div>
+</template>
+
+<script>
+	export default {
+		name: 'Administration'
+	};
+</script>
+
+<style scoped>
+
+</style>
