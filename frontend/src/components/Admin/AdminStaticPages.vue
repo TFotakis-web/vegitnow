7badd6977b9e6cfa5913ef29a4b77e360251984f
TFotakis <fotakistzanis@gmail.com>
2019-02-21 01:59:36 +0200

    Created AdminStaticPages component

---
+++ frontend/src/components/Admin/AdminStaticPages.vue
@@ -0,0 +1,17 @@
+<template>
+	<div id="AdminStaticPages" class="mb-5 h-100">
+		<div class="container">
+
+		</div>
+	</div>
+</template>
+
+<script>
+	export default {
+		name: 'AdminStaticPages'
+	};
+</script>
+
+<style scoped>
+
+</style>
