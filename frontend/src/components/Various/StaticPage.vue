3153d1f906bc7030f937ac0818dfe72f7618ddee
TFotakis <fotakistzanis@gmail.com>
2019-02-21 02:01:47 +0200

    Created StaticPage component

---
+++ frontend/src/components/Various/StaticPage.vue
@@ -0,0 +1,38 @@
+<template>
+	<div>
+		<div class="navbar-placeholder"></div>
+		<div class="container">
+			<div v-html="page.Content"></div>
+		</div>
+	</div>
+</template>
+
+<script>
+	export default {
+		name: 'StaticPage',
+		data: function () {
+			return {
+				id: this.$route.params.id,
+				page: {}
+			};
+		},
+		mounted: function () {
+			this.getStaticPageContent();
+		},
+		methods: {
+			getStaticPageContent: function () {
+				this.$http.get('/api/staticPage/' + this.id + '/')
+					.then((response) => {
+						this.page = response.data;
+					})
+					.catch((err) => {
+						console.log(err);
+					});
+			}
+		}
+	};
+</script>
+
+<style scoped>
+
+</style>
