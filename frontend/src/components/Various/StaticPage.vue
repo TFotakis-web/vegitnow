24bc6a215ad39d85732267f00fb3d71b24c8286e
TFotakis <fotakistzanis@gmail.com>
2019-02-26 12:22:40 +0200

    Implemented loader on StaticPage component

--- frontend/src/components/Various/StaticPage.vue
+++ frontend/src/components/Various/StaticPage.vue
@@ -1,19 +1,29 @@
 <template>
-	<div>
-		<div class="navbar-placeholder"></div>
-		<div class="container">
-			<div v-html="page.Content"></div>
+	<div class="d-flex flex-grow-1">
+		<Loader v-if="requestsUnsatisfied"/>
+		<div v-if="!requestsUnsatisfied" class="flex-grow-1">
+			<div class="navbar-placeholder"></div>
+			<div class="container">
+				<div v-html="page.Content"></div>
+			</div>
 		</div>
 	</div>
 </template>
 
 <script>
+	import Loader from '../Structure/Loader';
+
 	export default {
 		name: 'StaticPage',
+		components: {
+			Loader
+		},
 		data: function () {
 			return {
 				id: this.$route.params.id,
-				page: {}
+				page: {},
+				requestsUnsatisfied: 0
+
 			};
 		},
 		mounted: function () {
@@ -21,9 +31,11 @@
 		},
 		methods: {
 			getStaticPageContent: function () {
+				this.requestsUnsatisfied++;
 				this.$http.get('/api/staticPage/' + this.id + '/')
 					.then((response) => {
 						this.page = response.data;
+						this.requestsUnsatisfied--;
 					})
 					.catch((err) => {
 						console.log(err);
