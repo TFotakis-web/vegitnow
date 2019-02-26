9ec91b8772de7bc46a6aa81eeb70e71ba359981e
TFotakis <fotakistzanis@gmail.com>
2019-02-26 12:23:17 +0200

    Implemented loader on RecipesList component

--- frontend/src/components/Recipes/RecipesList.vue
+++ frontend/src/components/Recipes/RecipesList.vue
@@ -1,10 +1,13 @@
 <template>
-	<div>
-		<div class="navbar-placeholder"></div>
-		<div class="container">
-			<h1>{{ $t('Recipes') }}</h1>
-			<div class="row">
-				<RecipeCard v-for="recipe in recipes" :key="recipe.id" :recipe="recipe"></RecipeCard>
+	<div class="d-flex flex-grow-1">
+		<Loader v-if="requestsUnsatisfied"/>
+		<div v-if="!requestsUnsatisfied" class="flex-grow-1">
+			<div class="navbar-placeholder"></div>
+			<div class="container">
+				<h1>{{ $t('Recipes') }}</h1>
+				<div class="row">
+					<RecipeCard v-for="recipe in recipes" :key="recipe.id" :recipe="recipe"></RecipeCard>
+				</div>
 			</div>
 		</div>
 	</div>
@@ -12,42 +15,51 @@
 
 <script>
 	import RecipeCard from './RecipeCard';
+	import Loader from '../Structure/Loader';
 
 	export default {
 		name: 'RecipesList',
 		components: {
-			RecipeCard
+			RecipeCard,
+			Loader
 		},
 		data: function () {
 			return {
 				articleList: [],
 				articleContentTranslation: [],
-				articleTypeAssociation: []
+				articleTypeAssociation: [],
+				requestsUnsatisfied: 0
 			};
 		},
 		methods: {
 			getArticles: function () {
+				this.requestsUnsatisfied++;
 				this.$http.get('/api/article/')
 					.then((response) => {
 						this.articleList = response.data;
+						this.requestsUnsatisfied--;
 					})
 					.catch((err) => {
 						console.log(err);
 					});
 			},
 			getArticleContentTranslation: function () {
+				this.requestsUnsatisfied++;
 				this.$http.get('/api/articleContentTranslation/')
 					.then((response) => {
 						this.articleContentTranslation = response.data;
+						this.requestsUnsatisfied--;
 					})
 					.catch((err) => {
 						console.log(err);
 					});
 			},
 			getArticleTypeAssociation: function () {
+				this.requestsUnsatisfied++;
 				this.$http.get('/api/articleTypeAssociation/')
 					.then((response) => {
 						this.articleTypeAssociation = response.data;
+						this.requestsUnsatisfied--;
 					})
 					.catch((err) => {
 						console.log(err);
