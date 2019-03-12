19005aed207fd8a29a2ffca227420f7266cdde32
TFotakis <fotakistzanis@gmail.com>
2019-03-12 21:15:31 +0200

    Cleaned up settings.py and urls.py

--- vegitnow/settings.py
+++ vegitnow/settings.py
@@ -3,20 +3,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-# Progressive Web App functionality
-# PWA_SERVICE_WORKER_PATH = os.path.join(BASE_DIR, 'static/js', 'serviceworker.js')
-# PWA_APP_NAME = 'VegItNow'
-# PWA_APP_DESCRIPTION = 'VegItNow - All about vegans!'
-# PWA_APP_START_URL = '/'
-# PWA_APP_DISPLAY = 'standalone'
-# PWA_APP_BACKGROUND_COLOR = '#fff'
-# PWA_APP_THEME_COLOR = '#316e17'
-# PWA_APP_ICONS = [
-# 	{
-# 		'src': '/static/img/favicon.png',
-# 		'sizes': '157x157'
-# 	}
-# ]
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
@@ -29,7 +15,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
-INTERNAL_IPS = ['127.0.0.1']
+# INTERNAL_IPS = ['127.0.0.1']
 
 # Application definition
 
@@ -40,11 +26,9 @@ INSTALLED_APPS = [
 	'django.contrib.sessions',
 	'django.contrib.messages',
 	'django.contrib.staticfiles',
+	'django_extensions',
 	'django_summernote',
 	'rest_framework',
-	# 'pwa',
-	# 'webpack_loader',
-	# 'login',
 	'general',
 	'articles',
 ]
@@ -52,7 +36,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
 	'django.middleware.security.SecurityMiddleware',
 	'django.contrib.sessions.middleware.SessionMiddleware',
-	'django.middleware.locale.LocaleMiddleware',
+	# 'django.middleware.locale.LocaleMiddleware',
 	'django.middleware.common.CommonMiddleware',
 	'django.middleware.csrf.CsrfViewMiddleware',
 	'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -132,19 +116,19 @@ STATICFILES_DIRS = [
 	os.path.join(BASE_DIR, 'frontend/dist/static'),
 ]
 
-MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+# MEDIA_URL = '/media/'
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-from django.utils.translation import gettext_lazy as _
+# from django.utils.translation import gettext_lazy as _
 
-LANGUAGES = [
-	('el', _('Greek')),
-	('en', _('English')),
-]
-
-LOCALE_PATHS = (
-	os.path.join(BASE_DIR, 'locale'),
-)
+# LANGUAGES = [
+# 	('el', _('Greek')),
+# 	('en', _('English')),
+# ]
+#
+# LOCALE_PATHS = (
+# 	os.path.join(BASE_DIR, 'locale'),
+# )
 
 SUMMERNOTE_THEME = 'lite'
 
--- vegitnow/urls.py
+++ vegitnow/urls.py
@@ -1,34 +1,41 @@
-from django.conf import settings
-from django.conf.urls.static import static
-from django.contrib import admin
-from django.urls import path, re_path, include
-from django.views.decorators.cache import cache_control
-from django.views.generic import TemplateView
-
-from general.views import custom404View
-from .routers import router
-
-urlpatterns = [
-	path('s6AptmegHaGM3Ry5vdlr/database/', admin.site.urls),
-	path('api/', include(router.urls)),
-	path('summernote/', include('django_summernote.urls')),
-	path('service-worker.js', cache_control(max_age=2592000)(TemplateView.as_view(template_name="service-worker.js", content_type='application/javascript', )), name='service-worker.js'),
-]
-
-# if settings.DEBUG:
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-urlpatterns += [re_path(r'(?P<path>.*)', TemplateView.as_view(template_name='index.html'))]
-
+# from django.conf import settings
+# from django.conf.urls.static import static
+# from django.contrib import admin
+# from django.urls import path, re_path, include
+# from django.views.decorators.cache import cache_control
+# from django.views.generic import TemplateView
+#
+# from general.views import custom404View
+# from .routers import router
+#
+# urlpatterns = [
+# 	path('s6AptmegHaGM3Ry5vdlr/database/', admin.site.urls),
+# 	path('api/', include(router.urls)),
+# 	path('summernote/', include('django_summernote.urls')),
+# 	path('service-worker.js', cache_control(max_age=2592000)(TemplateView.as_view(template_name="service-worker.js", content_type='application/javascript', )), name='service-worker.js'),
+# ]
+#
+# # if settings.DEBUG:
+# urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+#
+# urlpatterns += [re_path(r'(?P<path>.*)', TemplateView.as_view(template_name='index.html'))]
+#
+# # urlpatterns += [
+# # path('', include(('general.urls', 'general'), namespace='general')),
+# # path('login/', include(('login.urls', 'login'), namespace='login')),
+# # path('articles/', include(('articles.urls', 'articles'), namespace='articles')),
+# # path('shop/', include(('shop.urls', 'shop'), namespace='shop')),
+# # path('', include('pwa.urls')),
+# # ]
+#
+#
 # urlpatterns += [
-# path('', include(('general.urls', 'general'), namespace='general')),
-# path('login/', include(('login.urls', 'login'), namespace='login')),
-# path('articles/', include(('articles.urls', 'articles'), namespace='articles')),
-# path('shop/', include(('shop.urls', 'shop'), namespace='shop')),
-# path('', include('pwa.urls')),
+# 	re_path(r'(?P<path>.*)', custom404View, name='custom404')
 # ]
 
+from django.contrib import admin
+from django.urls import path
 
-urlpatterns += [
-	re_path(r'(?P<path>.*)', custom404View, name='custom404')
+urlpatterns = [
+	path('admin/', admin.site.urls),
 ]
