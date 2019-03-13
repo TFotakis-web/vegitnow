38454c79a9a7f2592a60f439a17e2707a5a8bc41
TFotakis <fotakistzanis@gmail.com>
2019-03-13 16:53:43 +0200

    Cleaned up urls.py

--- vegitnow/urls.py
+++ vegitnow/urls.py
@@ -3,7 +3,6 @@ from django.urls import path, re_path, include
 from django.views.decorators.cache import cache_control
 from django.views.generic import TemplateView
 
-# from general.views import custom404View
 from .routers import router
 
 urlpatterns = [
@@ -11,22 +10,5 @@ urlpatterns = [
 	path('api/', include(router.urls)),
 	path('summernote/', include('django_summernote.urls')),
 	path('service-worker.js', cache_control(max_age=2592000)(TemplateView.as_view(template_name="service-worker.js", content_type='application/javascript', )), name='service-worker.js'),
+	re_path(r'(?P<path>.*)', TemplateView.as_view(template_name='index.html'))
 ]
-
-# if settings.DEBUG:
-# urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-urlpatterns += [re_path(r'(?P<path>.*)', TemplateView.as_view(template_name='index.html'))]
-
-# urlpatterns += [
-# path('', include(('general.urls', 'general'), namespace='general')),
-# path('login/', include(('login.urls', 'login'), namespace='login')),
-# path('articles/', include(('articles.urls', 'articles'), namespace='articles')),
-# path('shop/', include(('shop.urls', 'shop'), namespace='shop')),
-# path('', include('pwa.urls')),
-# ]
-
-
-# urlpatterns += [
-# 	re_path(r'(?P<path>.*)', custom404View, name='custom404')
-# ]
