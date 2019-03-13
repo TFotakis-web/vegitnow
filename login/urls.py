c7a091fb6e50fc91e16b7f6d6897c0c2f353e8f3
TFotakis <fotakistzanis@gmail.com>
2019-03-13 16:50:51 +0200

    Removed login app

--- login/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from . import views
-
-urlpatterns = [
-	path('', views.LoginFormView.as_view(), name='login'),
-	path('register/', views.RegisterFormView.as_view(), name='register'),
-	path('logout/', views.logoutView, name='logout'),
-]
