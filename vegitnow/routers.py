b3beee524ce9b6f2ef305ffaebb3fa7d071d4f89
TFotakis <fotakistzanis@gmail.com>
2019-03-14 23:12:15 +0200

    Removed unused viewsets from routers.py

--- vegitnow/routers.py
+++ vegitnow/routers.py
@@ -1,25 +1,41 @@
 from rest_framework import routers
 
-from articles.viewsets import ArticleViewSet, ArticleContentTranslationViewSet, ArticleTypeViewSet, ArticleTypeNameTranslationViewSet, ArticleTypeAssociationViewSet, TagViewSet, TagNameTranslationViewSet, TagAssociationViewSet, NewArticleViewSet, IngredientViewSet, IngredientNameTranslationViewSet, IngredientAssociationViewSet, UnitViewSet, UnitNameTranslationViewSet, NewIngredientViewSet
-from general.viewsets import LanguageViewSet, StaticPageViewSet, StaticPageTranslationViewSet
+from articles.viewsets import ArticleViewSet
+from articles.viewsets import ArticleTypeViewSet
+from articles.viewsets import IngredientViewSet
+# from articles.viewsets import ArticleContentTranslationViewSet
+# from articles.viewsets import ArticleTypeNameTranslationViewSet
+# from articles.viewsets import ArticleTypeAssociationViewSet
+# from articles.viewsets import TagViewSet
+# from articles.viewsets import TagNameTranslationViewSet
+# from articles.viewsets import TagAssociationViewSet
+# from articles.viewsets import IngredientNameTranslationViewSet
+# from articles.viewsets import IngredientAssociationViewSet
+# from articles.viewsets import UnitViewSet
+# from articles.viewsets import UnitNameTranslationViewSet
+# from articles.viewsets import NewIngredientViewSet
+# from articles.viewsets import NewArticleViewSet
+from general.viewsets import LanguageViewSet
+from general.viewsets import StaticPageViewSet
+from general.viewsets import StaticPageTranslationViewSet
 
 router = routers.DefaultRouter()
 
-router.register(r'language', LanguageViewSet)
 router.register(r'article', ArticleViewSet)
-router.register(r'articleContentTranslation', ArticleContentTranslationViewSet)
 router.register(r'articleType', ArticleTypeViewSet)
-router.register(r'articleTypeNameTranslation', ArticleTypeNameTranslationViewSet)
-router.register(r'articleTypeAssociation', ArticleTypeAssociationViewSet)
-router.register(r'tag', TagViewSet)
-router.register(r'tagNameTranslation', TagNameTranslationViewSet)
-router.register(r'tagAssociation', TagAssociationViewSet)
 router.register(r'ingredient', IngredientViewSet)
-router.register(r'ingredientNameTranslation', IngredientNameTranslationViewSet)
-router.register(r'ingredientAssociation', IngredientAssociationViewSet)
-router.register(r'unit', UnitViewSet)
-router.register(r'unitNameTranslation', UnitNameTranslationViewSet)
-router.register(r'newArticle', NewArticleViewSet, basename='newArticle')
-router.register(r'newIngredient', NewIngredientViewSet, basename='newIngredient')
-router.register(r'staticPage', StaticPageViewSet, basename='staticPage')
-router.register(r'staticPageTranslation', StaticPageTranslationViewSet, basename='staticPageTranslation')
+router.register(r'language', LanguageViewSet)
+router.register(r'staticPage', StaticPageViewSet)
+router.register(r'staticPageTranslation', StaticPageTranslationViewSet)
+# router.register(r'articleContentTranslation', ArticleContentTranslationViewSet)
+# router.register(r'articleTypeNameTranslation', ArticleTypeNameTranslationViewSet)
+# router.register(r'articleTypeAssociation', ArticleTypeAssociationViewSet)
+# router.register(r'tag', TagViewSet)
+# router.register(r'tagNameTranslation', TagNameTranslationViewSet)
+# router.register(r'tagAssociation', TagAssociationViewSet)
+# router.register(r'ingredientNameTranslation', IngredientNameTranslationViewSet)
+# router.register(r'ingredientAssociation', IngredientAssociationViewSet)
+# router.register(r'unit', UnitViewSet)
+# router.register(r'unitNameTranslation', UnitNameTranslationViewSet)
+# router.register(r'newArticle', NewArticleViewSet, basename='newArticle')
+# router.register(r'newIngredient', NewIngredientViewSet, basename='newIngredient')
