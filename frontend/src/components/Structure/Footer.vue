191b8a71fc5728ea3196831946436fe2a14c9529
TFotakis <fotakistzanis@gmail.com>
2019-03-17 20:33:32 +0200

    Implemented Subscribe to newsletter on footer component with mailchimp support

--- frontend/src/components/Structure/Footer.vue
+++ frontend/src/components/Structure/Footer.vue
@@ -15,13 +15,41 @@
 						<h1 class="fgGreen0">{{ $t('Join our Family') }}</h1>
 					</div>
 					<div class="col-md-3 mb-4">
-						<form class="form-group">
-							<input class="form-control mb-1" type="email" :placeholder="$t('Insert your email') + '...'" aria-label="email"
-							       style="border-bottom: solid #2d6c13 2px;border-top: 0; border-left: 0; border-right: 0;">
-							<button class="btn bgGreen0 text-white form-control" style="box-shadow: none" type="submit">
-								<i class="icon-mail"></i> {{ $t('Subscribe') }}
-							</button>
-						</form>
+						<!-- Begin Mailchimp Signup Form -->
+						<div id="mc_embed_signup">
+							<form @submit="subscribe" class="form-group validate" action="https://vegitnow.us20.list-manage.com/subscribe/post?u=cf57f59f1fada010f4b6dc6ba&amp;id=a6721a14ec" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" target="_blank" novalidate>
+								<div id="mc_embed_signup_scroll">
+									<div class="mc-field-group">
+										<!--<label for="mce-EMAIL">Email Address </label>-->
+										<input v-model="newsletterEmail" type="email" value="" name="EMAIL" class="form-control mb-1 required email" id="mce-EMAIL" style="border-bottom: solid #2d6c13 2px;border-top: 0; border-left: 0; border-right: 0;" :placeholder="$t('Insert your email') + '...'">
+									</div>
+									<div id="mce-responses" class="clear">
+										<div class="response" id="mce-error-response" style="display:none"></div>
+										<div class="response" id="mce-success-response" style="display:none"></div>
+									</div>
+									<!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+									<div style="position: absolute; left: -5000px;" aria-hidden="true">
+										<input type="text" name="b_cf57f59f1fada010f4b6dc6ba_a6721a14ec" tabindex="-1" value="">
+									</div>
+									<div class="clear">
+										<button class="btn bgGreen0 text-white form-control" type="submit" name="subscribe" id="mc-embedded-subscribe">
+											<i class="icon-mail"></i> {{ $t('Subscribe') }}
+										</button>
+									</div>
+								</div>
+							</form>
+						</div>
+
+						<!--End mc_embed_signup-->
+
+
+						<!--<form @submit="subscribe" action="#" class="form-group">-->
+							<!--<input v-model="newsletterEmail" class="form-control mb-1" type="email" :placeholder="$t('Insert your email') + '...'" aria-label="email"-->
+							       <!--style="border-bottom: solid #2d6c13 2px;border-top: 0; border-left: 0; border-right: 0;">-->
+							<!--<button class="btn bgGreen0 text-white form-control" style="box-shadow: none" type="submit">-->
+								<!--<i class="icon-mail"></i> {{ $t('Subscribe') }}-->
+							<!--</button>-->
+						<!--</form>-->
 					</div>
 					<div class="col-md-3 mb-4">
 						<div class="container-fluid">
@@ -35,8 +63,9 @@
 					<div class="col-md-3 mb-4">
 						<h1 class="fgGreen0">{{ $t('Follow us') }}</h1>
 						<h1>
-							<a class="fgGreen0" href="#"><span class="icon-facebook"></span></a>
-							<a class="fgGreen0" href="#"><span class="icon-instagram"></span></a>
+							<a class="fgGreen0" href="https://www.facebook.com/vegitnow/" target="_blank"><span class="icon-facebook"></span></a>
+							<a class="fgGreen0" href="https://www.instagram.com/vegitnow/" target="_blank"><span class="icon-instagram"></span></a>
+							<a class="fgGreen0" href="https://www.youtube.com/channel/UC_V2b4B_sK7c4fB28KK-4pw" target="_blank"><span class="icon-youtube-play"></span></a>
 						</h1>
 					</div>
 				</div>
@@ -50,7 +79,8 @@
 		name: 'Footer',
 		data: function () {
 			return {
-				staticPages: {}
+				staticPages: {},
+				newsletterEmail: ''
 			};
 		},
 		mounted: function () {
@@ -61,6 +91,22 @@
 				this.$http.get('/api/staticPage/?locale=' + this.$cookie.get('locale'))
 					.then((response) => {
 						this.staticPages = response.data;
+					})
+					.catch((err) => {
+						console.log(err);
+						this.$notify({
+							text: this.$t('Something went wrong... Please check your connection.'),
+							type: 'error'
+						});
+					});
+			},
+			subscribe: function () {
+				this.$http.post('/api/createNewsletterUser/', {email: this.newsletterEmail})
+					.then((response) => {
+						this.$notify({
+							text: this.$t('Subscribed successfully!'),
+							type: 'success'
+						});
 					})
 					.catch((err) => {
 						console.log(err);
