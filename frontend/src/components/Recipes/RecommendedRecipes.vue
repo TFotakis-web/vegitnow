2061910fbf682f38a491b56fc1c1620ad15b4566
TFotakis <fotakistzanis@gmail.com>
2019-03-17 20:55:52 +0200

    Show RecommendedRecipes component if there are available recipes

--- frontend/src/components/Recipes/RecommendedRecipes.vue
+++ frontend/src/components/Recipes/RecommendedRecipes.vue
@@ -1,5 +1,5 @@
 <template>
-	<div id="recommendedRecipes">
+	<div id="recommendedRecipes" v-if="recipes.length">
 		<div id="RecipesTripleCard" class="d-none d-xl-block" style="position: relative;">
 			<a class="btn btn-circle bgGreen2 text-white" href="#recipeCarouselTripleCard" role="button" data-slide="prev" style="
 				   font-size: 1.5rem;
