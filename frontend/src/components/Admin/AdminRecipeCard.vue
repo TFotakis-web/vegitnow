4d2f0f19559f35295c2e953c3e18dc5d037f5d91
TFotakis <fotakistzanis@gmail.com>
2019-03-19 18:05:03 +0200

    Implemented deleting of a recipe in AdminRecipeCard component

--- frontend/src/components/Admin/AdminRecipeCard.vue
+++ frontend/src/components/Admin/AdminRecipeCard.vue
@@ -18,7 +18,7 @@
 		<div class="row py-4">
 			<router-link :to="{ name: 'RecipeView', params: { id: recipe.id }}" class="btn px-4 bgGreen0 text-white text-uppercase font-weight-bold mx-auto" style="border-radius: 2rem;">{{ $t('Read more') }}</router-link>
 			<router-link :to="{ name: 'RecipeView', params: { id: recipe.id }}" class="btn px-4 bg-info text-white text-uppercase font-weight-bold mx-auto" style="border-radius: 2rem;">{{ $t('Edit') }}</router-link>
-			<router-link :to="{ name: 'RecipeView', params: { id: recipe.id }}" class="btn px-4 bg-danger text-white text-uppercase font-weight-bold mx-auto" style="border-radius: 2rem;">{{ $t('Delete') }}</router-link>
+			<button @click="deleteArticle()" class="btn px-4 bg-danger text-white text-uppercase font-weight-bold mx-auto" style="border-radius: 2rem;">{{ $t('Delete') }}</button>
 		</div>
 	</div>
 </template>
@@ -29,7 +29,22 @@
 		props: [
 			'recipe',
 			'isMiddle'
-		]
+		],
+		methods: {
+			deleteArticle: function () {
+				this.$http.delete('/api/article/' + this.recipe['ArticleContentTranslationId'] + '/')
+					.then((response) => {
+						this.$parent.getArticles();
+					})
+					.catch((err) => {
+						console.log(err);
+						this.$notify({
+							text: this.$t('Something went wrong... Please check your connection.'),
+							type: 'error'
+						});
+					});
+			}
+		}
 	};
 </script>
 
