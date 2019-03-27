c63824c661b1217eea2c28f0e6818ad3c1d0bf05
TFotakis <fotakistzanis@gmail.com>
2019-03-27 14:20:36 +0200

    Added static urls

--- vegitnow/urls.py
+++ vegitnow/urls.py
@@ -7,16 +7,14 @@ from django.views.generic import TemplateView
 from vegitnow import settings
 from .routers import router
 
-
 urlpatterns = [
 	path('s6AptmegHaGM3Ry5vdlr/database/', admin.site.urls),
 	path('api/', include(router.urls)),
 	path('summernote/', include('django_summernote.urls')),
 	path('service-worker.js', cache_control(max_age=2592000)(TemplateView.as_view(template_name="service-worker.js", content_type='application/javascript', )), name='service-worker.js'),
-	# re_path(r'(?P<path>.*)', TemplateView.as_view(template_name='index.html')),
 ]
 
-if settings.DEBUG:
-	urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-urlpatterns.append(re_path(r'(?P<path>.*)', TemplateView.as_view(template_name='index.html')))
+urlpatterns += [re_path(r'(?P<path>.*)', TemplateView.as_view(template_name='index.html'))]
