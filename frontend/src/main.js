5c7b1db5e8a192a8ca6ceef23c571076e6570c54
TFotakis <fotakistzanis@gmail.com>
2019-04-30 16:01:49 +0300

    Removed getCookie to use VueCookies.get on main.js

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -109,21 +109,5 @@ new Vue({
 	}
 });
 
-let getCookie = name => {
-	let cookieValue = null;
-	if (document.cookie && document.cookie !== '') {
-		const cookies = document.cookie.split(';');
-		for (let i = 0; i < cookies.length; i++) {
-			const cookie = jQuery.trim(cookies[i]);
-			// Does this cookie string begin with the name we want?
-			if (cookie.substring(0, name.length + 1) === (name + '=')) {
-				cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
-				break;
-			}
-		}
-	}
-	return cookieValue;
-};
-
-Vue.http.headers.common['X-CSRFToken'] = getCookie('csrftoken');
-Vue.http.headers.common['locale'] = getCookie('locale');
+Vue.http.headers.common['X-CSRFToken'] = VueCookie.get('csrftoken');
+Vue.http.headers.common['locale'] = VueCookie.get('locale');
