0a4999a2903068aa2972d52231636e380a3d2ec7
TFotakis <fotakistzanis@gmail.com>
2019-05-02 00:07:51 +0300

    Added AdViewSet on routers - Cleaned up routers

--- vegitnow/routers.py
+++ vegitnow/routers.py
@@ -5,18 +5,12 @@ from articles.viewsets import ArticleViewSet
 from articles.viewsets import IngredientAssociationViewSet
 from articles.viewsets import IngredientViewSet
 from articles.viewsets import ArticleContentTranslationViewSet
-# from articles.viewsets import ArticleTypeNameTranslationViewSet
-# from articles.viewsets import ArticleTypeAssociationViewSet
-# from articles.viewsets import IngredientNameTranslationViewSet
-# from articles.viewsets import TagViewSet
-# from articles.viewsets import TagNameTranslationViewSet
-# from articles.viewsets import TagAssociationViewSet
-# from articles.viewsets import UnitViewSet
-# from articles.viewsets import UnitNameTranslationViewSet
-from general.viewsets import LanguageViewSet, CreateNewsletterUserViewSet
+from general.viewsets import LanguageViewSet
+from general.viewsets import CreateNewsletterUserViewSet
 from general.viewsets import NewsletterUserViewSet
 from general.viewsets import StaticPageTranslationViewSet
 from general.viewsets import StaticPageViewSet
+from general.viewsets import AdViewSet
 
 router = routers.DefaultRouter()
 
@@ -30,11 +24,4 @@ router.register(r'newsletterUser', NewsletterUserViewSet)
 router.register(r'createNewsletterUser', CreateNewsletterUserViewSet, basename='createNewsletterUser')
 router.register(r'ingredientAssociation', IngredientAssociationViewSet)
 router.register(r'articleContentTranslation', ArticleContentTranslationViewSet)
-# router.register(r'articleTypeNameTranslation', ArticleTypeNameTranslationViewSet)
-# router.register(r'articleTypeAssociation', ArticleTypeAssociationViewSet)
-# router.register(r'ingredientNameTranslation', IngredientNameTranslationViewSet)
-# router.register(r'tag', TagViewSet)
-# router.register(r'tagNameTranslation', TagNameTranslationViewSet)
-# router.register(r'tagAssociation', TagAssociationViewSet)
-# router.register(r'unit', UnitViewSet)
-# router.register(r'unitNameTranslation', UnitNameTranslationViewSet)
+router.register(r'vegitnowad', AdViewSet)
