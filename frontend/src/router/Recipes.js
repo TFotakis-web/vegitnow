3575df09b4a21523f28cdd6f9f019329abbf984e
TFotakis <fotakistzanis@gmail.com>
2019-05-02 00:09:39 +0300

    Reformatted router Recipes.js

--- frontend/src/router/Recipes.js
+++ frontend/src/router/Recipes.js
@@ -1,17 +1,6 @@
-import RecipesList from '../components/Recipes/RecipesList';
-import RecipeView from '../components/Recipes/RecipeView';
-
 const basePath = 'recipes/';
 
 export default [
-	{
-		path: basePath,
-		name: 'RecipesList',
-		component: RecipesList
-	},
-	{
-		path: basePath + ':id/',
-		name: 'RecipeView',
-		component: RecipeView
-	}
+	{ path: basePath, name: 'RecipesList', component: require('../components/Recipes/RecipesList').default },
+	{ path: basePath + ':id/', name: 'RecipeView', component: require('../components/Recipes/RecipeView').default }
 ];
