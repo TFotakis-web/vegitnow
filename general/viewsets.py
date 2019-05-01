911349f919b9d22eab44d2f07ea414648def7d43
TFotakis <fotakistzanis@gmail.com>
2019-05-02 00:07:16 +0300

    Created AdViewSet on general viewsets

--- general/viewsets.py
+++ general/viewsets.py
@@ -1,11 +1,22 @@
+from datetime import datetime
+
 from rest_framework import viewsets, mixins
 from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAdminUser
 from rest_framework.response import Response
 from rest_framework.status import *
 from rest_framework.viewsets import GenericViewSet
 
-from .models import Language, StaticPage, StaticPageTranslation, NewsletterUser
-from .serializers import LanguageSerializer, StaticPageSerializer, StaticPageTranslationSerializer, NewsletterUserSerializer
+from articles.viewsets import base64toFile
+from general.models import Ad, AdTranslation
+from general.models import Language
+from general.models import NewsletterUser
+from general.models import StaticPage
+from general.models import StaticPageTranslation
+from general.serializers import AdSerializer
+from general.serializers import LanguageSerializer
+from general.serializers import NewsletterUserSerializer
+from general.serializers import StaticPageSerializer
+from general.serializers import StaticPageTranslationSerializer
 
 
 class LanguageViewSet(viewsets.ModelViewSet):
@@ -168,6 +179,124 @@ class NewsletterUserViewSet(viewsets.ModelViewSet):
 
 
 class CreateNewsletterUserViewSet(mixins.CreateModelMixin, GenericViewSet):
-	# queryset = NewsletterUser.objects.all()
 	serializer_class = NewsletterUserSerializer
-	# permission_classes = (IsAdminUser,)
+
+
+class AdViewSet(viewsets.ModelViewSet):
+	queryset = Ad.objects.all()
+	serializer_class = AdSerializer
+	permission_classes = (IsAuthenticatedOrReadOnly,)
+
+	def list(self, request, *args, **kwargs):
+		if 'id' in request.query_params:
+			ad = Ad.objects.get(id=int(request.query_params['id']))
+			ad.Clicks += 1
+			ad.save()
+			return Response(status=HTTP_200_OK)
+
+		if request.user.is_superuser and 'admin' in request.query_params:
+			ads = Ad.objects.all()
+			resp = []
+			for ad in ads:
+				translations = []
+				for translation in ad.adtranslation_set.all():
+					translations.append({
+						'id': translation.id,
+						'Language': translation.Language.id,
+						'Image': {
+							'name': '',
+							'data': ''
+						},
+						'Link': translation.Link,
+					})
+				resp.append({
+					'id': ad.id,
+					'Name': ad.Name,
+					'AdType': ad.AdType,
+					'Clicks': ad.Clicks,
+					'TargetClicks': ad.TargetClicks,
+					'ReleaseDateTime': {
+						'date': ad.ReleaseDateTime.strftime('%d/%m/%Y'),
+						'time': ad.ReleaseDateTime.strftime('%H:%M')
+					},
+					'RemoveDateTime': {
+						'date': ad.RemoveDateTime.strftime('%d/%m/%Y'),
+						'time': ad.RemoveDateTime.strftime('%H:%M')
+					},
+					'Translations': translations
+				})
+		else:
+			f = {}
+			if 'type' in request.query_params:
+				f['AdType'] = request.query_params['type']
+
+			ads = Ad.objects.filter(**f).all()
+			resp = []
+			f = {}
+			if 'locale' in request.query_params:
+				f['Language_id'] = int(request.query_params['locale'])
+			for ad in ads:
+				if (not request.user.is_superuser) and not ad.Active:
+					continue
+				translation = ad.adtranslation_set.filter(**f).first()
+				if not translation:
+					continue
+				resp.append({
+					'id': ad.id,
+					'Image': translation.Image.url,
+					'Link': translation.Link,
+				})
+		return Response(resp)
+
+	def create(self, request, *args, **kwargs):
+		ad = Ad(
+			Name=request.data['Name'],
+			AdType=request.data['AdType'],
+			TargetClicks=request.data['TargetClicks'],
+			ReleaseDateTime=datetime.strptime(request.data['ReleaseDateTime']['date'] + '-' + request.data['ReleaseDateTime']['time'] + '-' + 'UTC', '%d/%m/%Y-%H:%M-%Z'),
+			RemoveDateTime=datetime.strptime(request.data['RemoveDateTime']['date'] + '-' + request.data['RemoveDateTime']['time'] + '-' + 'UTC', '%d/%m/%Y-%H:%M-%Z'),
+		)
+		ad.save()
+		for translation in request.data['Translations']:
+			adTranslation = AdTranslation(
+				Ad=ad,
+				Language_id=translation['Language'],
+				Image=base64toFile(translation['Image']['name'], translation['Image']['data']),
+				Link=translation['Link']
+			)
+			adTranslation.save()
+		return Response(status=HTTP_200_OK)
+
+	def update(self, request, *args, **kwargs):
+		try:
+			ad = Ad.objects.get(id=kwargs['pk'])
+		except:
+			return Response(status=HTTP_404_NOT_FOUND)
+		ad.Name = request.data['Name']
+		ad.AdType = request.data['AdType']
+		ad.TargetClicks = request.data['TargetClicks']
+		ad.ReleaseDateTime = datetime.strptime(request.data['ReleaseDateTime']['date'] + '-' + request.data['ReleaseDateTime']['time'] + '-' + 'UTC', '%d/%m/%Y-%H:%M-%Z')
+		ad.RemoveDateTime = datetime.strptime(request.data['RemoveDateTime']['date'] + '-' + request.data['RemoveDateTime']['time'] + '-' + 'UTC', '%d/%m/%Y-%H:%M-%Z')
+		ad.save()
+
+		for translation in request.data['Translations']:
+			if translation['id'] == -1:
+				adTranslation = AdTranslation()
+			else:
+				try:
+					adTranslation = AdTranslation.objects.get(id=translation['id'])
+				except:
+					continue
+			adTranslation.Ad = ad
+			adTranslation.Language_id = translation['Language']
+			if translation['Image']['data']:
+				adTranslation.Image = base64toFile(translation['Image']['name'], translation['Image']['data'])
+			adTranslation.Link = translation['Link']
+			adTranslation.save()
+		return Response(status=HTTP_200_OK)
+
+	def destroy(self, request, *args, **kwargs):
+		if 'translationId' not in request.query_params:
+			return super().destroy(request)
+		AdTranslation.objects.filter(id=request.query_params['translationId']).delete()
+		return Response(status=HTTP_200_OK)
