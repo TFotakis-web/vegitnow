7301de0bea6b1ff00ad22d2cdd846738c9e012a8
TFotakis <fotakistzanis@gmail.com>
2019-05-02 00:04:25 +0300

    Created AdTranslation, AdTypeEnum and Ad models on general models

--- general/models.py
+++ general/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from datetime import datetime
+from enum import Enum
 
 
 class Language(models.Model):
@@ -32,3 +34,40 @@ class NewsletterUser(models.Model):
 
 	def __str__(self):
 		return self.email
+
+
+class AdTranslation(models.Model):
+	Ad = models.ForeignKey('Ad', blank=True, null=True, on_delete=models.CASCADE)
+	Language = models.ForeignKey(Language, on_delete=models.DO_NOTHING)
+	Image = models.ImageField(blank=True)
+	Link = models.CharField(default='', max_length=2048)
+
+	def __str__(self): return self.Ad.Name + ': ' + self.Language.Code
+
+
+class AdTypeEnum(Enum):
+	HOME_JOIN_OUR_FAMILY = 'Home: Join Our Family'
+	HOME_ARTICLES = 'Home: Articles'
+	HOME_RECIPES = 'Home: Recipes'
+	INSIDE_POST = 'Inside: Post'
+
+
+class Ad(models.Model):
+	Name = models.CharField(default='', max_length=100)
+	AdType = models.CharField(
+		max_length=50,
+		choices=[(tag.name, tag.value) for tag in AdTypeEnum]
+	)
+	Clicks = models.IntegerField(default=0, blank=True)
+	TargetClicks = models.IntegerField(default=0, blank=True)
+	ReleaseDateTime = models.DateTimeField(default=None, blank=True, null=True)
+	RemoveDateTime = models.DateTimeField(default=None, blank=True, null=True)
+
+	@property
+	def Active(self):
+		released = self.ReleaseDateTime is None or self.ReleaseDateTime.utctimetuple() <= datetime.now().astimezone().utctimetuple()
+		clicksRemaining = self.Clicks < self.TargetClicks
+		removed = self.RemoveDateTime.utctimetuple() <= datetime.now().astimezone().utctimetuple()
+		return released and clicksRemaining and not removed
+
+	def __str__(self): return self.Name
