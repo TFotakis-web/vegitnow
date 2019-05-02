9ba29c4377231d420708b078dd11ca76b32fa9e8
TFotakis <fotakistzanis@gmail.com>
2019-05-02 18:55:46 +0300

    Created AdCard component

---
+++ frontend/src/components/Structure/Ads/AdCard.vue
@@ -0,0 +1,30 @@
+<template>
+	<div class="col-12 col-lg-6 mb-3 text-center" :class="[this.listType === 'recipes'? 'col-xl-4' : '']">
+		<AdImg :ad="ad"/>
+	</div>
+</template>
+
+<script>
+	import AdImg from './AdImg';
+
+	export default {
+		name: 'AdCard',
+		components: {
+			AdImg
+		},
+		props: {
+			ad: {
+				type: Object,
+				default: {}
+			},
+			listType: {
+				type: String,
+				default: ''
+			}
+		}
+	};
+</script>
+
+<style scoped>
+
+</style>
