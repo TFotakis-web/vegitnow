85362fccfc76ece46fcd4b0f37881cbd97be3c8e
TFotakis <fotakistzanis@gmail.com>
2019-05-02 18:55:35 +0300

    Created AdImg component

---
+++ frontend/src/components/Structure/Ads/AdImg.vue
@@ -0,0 +1,26 @@
+<template>
+	<a :href="ad.Link" @mousedown="countUp(ad.id)" target="_blank" rel="noopener" class="d-inline-block">
+		<img :src="ad.Image" :alt="ad.Image" class="img-fluid" style="max-height: 250px">
+	</a>
+</template>
+
+<script>
+	export default {
+		name: 'AdImg',
+		props: {
+			ad: {
+				type: Object,
+				default: {}
+			}
+		},
+		methods: {
+			countUp: function () {
+				this.$http.get('/api/vegitnowad/?id=' + this.ad.id);
+			}
+		}
+	};
+</script>
+
+<style scoped>
+
+</style>
