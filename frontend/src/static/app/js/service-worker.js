3f9975e1628aa6d6f5aed2a7668bc35ae14fc368
TFotakis <fotakistzanis@gmail.com>
2019-07-14 00:55:18 +0300

    Implemented service-worker.js with static/media/api caches and url cacheFirst and networkFirst strategies

--- frontend/src/static/app/js/service-worker.js
+++ frontend/src/static/app/js/service-worker.js
@@ -1,48 +1,138 @@
-console.log("Running");
+/**
+ * Service Worker version 20190713
+ */
 
-const staticCacheName = 'vegitnow-static-v2';
+let today = new Date();
+let timestamp = today.getFullYear().toString();
+timestamp += (today.getMonth() < 8 ? '0' : '') + (today.getMonth() + 1).toString();
+timestamp += today.getDate().toString();
+
+const staticCacheName = 'static-' + timestamp;
+const mediaCacheName = 'media-20190713';
+const apiCacheName = 'api-' + timestamp;
+const cacheWhitelist = [staticCacheName, mediaCacheName, apiCacheName];
+
 const precacheUrls = [
 	'/'
 ];
 
-self.addEventListener('install', event => {
-	self.skipWaiting();
-	event.waitUntil(
+if (typeof self.skipWaiting === 'function') {
+	self.addEventListener('install', function (e) {
+		e.waitUntil(self.skipWaiting());
+	});
+}
+
+self.addEventListener('activate', function (e) {
+	if (self.clients && (typeof self.clients.claim === 'function')) {
+		// self.clients.claim();
+		e.waitUntil(self.clients.claim());
+	}
+	e.waitUntil(
+		caches.keys().then(function (cacheNames) {
+			return Promise.all(
+				cacheNames.map(function (cacheName) {
+					if (cacheWhitelist.indexOf(cacheName) === -1) {
+						caches.delete(cacheName);
+						// console.log('Deleted cache: ', cacheName);
+					}
+				})
+			);
+		})
+	);
+	e.waitUntil(
 		caches.open(staticCacheName).then(cache => {
 			return cache.addAll(precacheUrls);
 		})
 	);
 });
 
-self.addEventListener('activate', event => {
-	event.waitUntil(
-		caches
-			.keys()
-			.then(cacheNames => {
-				return Promise.all(
-					cacheNames
-						.filter(cacheName => {
-							return cacheName.startsWith('vegitnow-') && cacheName !== staticCacheName;
-						})
-						.map(cacheName => {
-							caches.delete(cacheName);
-							console.log('Deleted cache: ', cacheName)
-						})
-				);
+let networkFirst = function (event, cacheToUse) {
+	event.respondWith(
+		fetch(event.request)
+			.then(function (response) {
+				if (!response || response.status !== 200 || response.type !== 'basic') return response;
+
+				let responseClone = response.clone();
+				caches.open(cacheToUse)
+					.then(function (cache) {
+						cache.put(event.request, responseClone);
+					});
+				return response;
+			})
+			.catch(function () {
+				return caches.open(cacheToUse)
+					.then(function (cache) {
+						return cache.match(event.request);
+					});
 			})
 	);
-});
+};
+
+let cacheFirst = function (event, cacheToUse) {
+	event.respondWith(
+		caches.open(cacheToUse)
+			.then(function (cache) {
+				return cache.match(event.request)
+					.then(function (response) {
+						return response ||
+							fetch(event.request)
+								.then(function (response) {
+										if (!response || response.status !== 200 || response.type !== 'basic') return response;
+
+										cache.put(event.request, response.clone());
+										return response;
+									}
+								);
+					});
+			})
+	);
+};
 
 self.addEventListener('fetch', event => {
-	// console.log('Service worker: ', event.request);
-	event.respondWith(
-		caches.open(staticCacheName)
-			.then(cache => {
-				return fetch(event.request)
-					.then(function (response) {
-						cache.put(event.request, response.clone());
-						return response;
-					}) || cache.match(event.request);
-			})
-	)
+	const requestURL = new URL(event.request.url);
+	if (self.location.origin !== requestURL.origin) return;
+	else if (/^\/__webpack_hmr/.test(requestURL.pathname)) return;
+	else if (/^\/static\//.test(requestURL.pathname)) cacheFirst(event, staticCacheName);
+	else if (/^\/media\//.test(requestURL.pathname)) cacheFirst(event, mediaCacheName);
+	else if (/^\/api\//.test(requestURL.pathname)) networkFirst(event, apiCacheName);
+	else {
+		// event.respondWith(
+		// 	caches.match('/')
+		// 		.then(function (response) {
+		// 			return response ||
+		// 				fetch(new Request('/'))
+		// 					.then(function (response) {
+		// 							if (!response || response.status !== 200 || response.type !== 'basic') return response;
+		// 							let responseClone = response.clone();
+		// 							caches.open(staticCacheName)
+		// 								.then(function (cache) {
+		// 									cache.put('/', responseClone);
+		// 								});
+		// 							return response;
+		// 						}
+		// 					);
+		// 		})
+		// );
+		let requestRoot = new Request('/');
+		let cacheToUse = staticCacheName;
+		event.respondWith(
+			fetch(requestRoot)
+				.then(function (response) {
+					if (!response || response.status !== 200 || response.type !== 'basic') return response;
+
+					let responseClone = response.clone();
+					caches.open(cacheToUse)
+						.then(function (cache) {
+							cache.put(requestRoot, responseClone);
+						});
+					return response;
+				})
+				.catch(function () {
+					return caches.open(cacheToUse)
+						.then(function (cache) {
+							return cache.match(requestRoot);
+						});
+				})
+		);
+	}
 });
