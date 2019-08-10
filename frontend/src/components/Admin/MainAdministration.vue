ca146355134499d65c0ee65913879809ee04ba9c
TFotakis <fotakistzanis@gmail.com>
2019-08-10 23:35:48 +0300

    Added tabs on MainAdministration component

--- frontend/src/components/Admin/MainAdministration.vue
+++ frontend/src/components/Admin/MainAdministration.vue
@@ -1,15 +1,15 @@
 <template>
 	<div id="MainAdministration" class="d-flex flex-column flex-grow-1">
-		<div class="navbar-placeholder"></div>
-		<div class="container">
-			<div class="row">
-				<!--<h1 class="text-center">Main Administration</h1>-->
-				<router-link :to="{ name: 'Administration' }" class="col text-right">{{ $t('Administration') }}</router-link>
-<!--				<router-link :to="{ name: 'DatabaseAdministration' }" class="col" target="_blank">{{ $t('Database') }}</router-link>-->
-				<a href="/s6AptmegHaGM3Ry5vdlr/database/" class="col">{{ $t('Database') }}</a>
-			</div>
+		<div class="container my-3">
+			<ul class="nav nav-tabs">
+				<li class="nav-item">
+					<router-link :to="{ name: 'Administration' }" class="nav-link">{{ $t('Administration') }}</router-link>
+				</li>
+				<li class="nav-item">
+					<a href="/s6AptmegHaGM3Ry5vdlr/database/" class="nav-link">{{ $t('Database') }}</a>
+				</li>
+			</ul>
 		</div>
-		<hr>
 		<router-view/>
 	</div>
 </template>
