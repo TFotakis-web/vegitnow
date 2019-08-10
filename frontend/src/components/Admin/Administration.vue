689c4ab9efa60aa1b3570064c58f5563a1e1b5a5
TFotakis <fotakistzanis@gmail.com>
2019-08-10 23:36:13 +0300

    Added tabs on Administration component

--- frontend/src/components/Admin/Administration.vue
+++ frontend/src/components/Admin/Administration.vue
@@ -1,24 +1,27 @@
 <template>
 	<div id="Administration" class="d-flex flex-column flex-grow-1">
-		<!--<div class="navbar-placeholder"></div>-->
 		<div class="container h-100">
-			<div class="row text-center">
-				<!--<h1 class="text-center">Administration</h1>-->
-				<router-link :to="{ name: 'CreateArticle' }" class="col">{{ $t('Create New') }}</router-link>
-				<router-link :to="{ name: 'ArticleList' }" class="col">{{ $t('Articles') }}</router-link>
-				<router-link :to="{ name: 'RecipeList' }" class="col">{{ $t('Recipes') }}</router-link>
-				<router-link :to="{ name: 'IngredientList' }" class="col">{{ $t('Ingredients') }}</router-link>
-				<router-link :to="{ name: 'StaticPages' }" class="col">{{ $t('Static Pages') }}</router-link>
-				<router-link :to="{ name: 'Ads' }" class="col">{{ $t('Ads') }}</router-link>
-			</div>
-			<div class="row">
-				<hr>
-			</div>
-			<div class="row h-100">
-				<!--<transition name="fade">-->
-				<router-view/>
-				<!--</transition>-->
-			</div>
+			<ul class="nav nav-tabs mb-3">
+				<li class="nav-item">
+					<router-link :to="{ name: 'CreateArticle' }" class="nav-link">{{ $t('Create New') }}</router-link>
+				</li>
+				<li class="nav-item">
+					<router-link :to="{ name: 'ArticleList' }" class="nav-link">{{ $t('Articles') }}</router-link>
+				</li>
+				<li class="nav-item">
+					<router-link :to="{ name: 'RecipeList' }" class="nav-link">{{ $t('Recipes') }}</router-link>
+				</li>
+				<li class="nav-item">
+					<router-link :to="{ name: 'IngredientList' }" class="nav-link">{{ $t('Ingredients') }}</router-link>
+				</li>
+				<li class="nav-item">
+					<router-link :to="{ name: 'StaticPages' }" class="nav-link">{{ $t('Static Pages') }}</router-link>
+				</li>
+				<li class="nav-item">
+					<router-link :to="{ name: 'Ads' }" class="nav-link">{{ $t('Ads') }}</router-link>
+				</li>
+			</ul>
+			<router-view/>
 		</div>
 	</div>
 </template>
