d26b4136d8fda7b719cb9c39a770a0052f9f5c01
TFotakis <fotakistzanis@gmail.com>
2019-08-15 02:32:26 +0300

    Added froala urls

--- vegitnow/urls.py
+++ vegitnow/urls.py
@@ -1,3 +1,4 @@
+from django.conf.urls import url
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, re_path, include
@@ -12,6 +13,7 @@ urlpatterns = [
 	path('s6AptmegHaGM3Ry5vdlr/database/', admin.site.urls),
 	path('api/', include(router.urls)),
 	path('summernote/', include('django_summernote.urls')),
+	url(r'^froala_editor/', include('froala_editor.urls')),
 	path('service-worker.js', cache_control(max_age=2592000)(TemplateView.as_view(template_name="static/app/js/service-worker.js", content_type='application/javascript', )), name='service-worker.js'),
 ]
 
