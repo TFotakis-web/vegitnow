1c520ddb8b29cafeb583436ef100d83093ef9237
TFotakis <fotakistzanis@gmail.com>
2019-08-15 02:35:40 +0300

    Replaced TextField with FroalaField on StaticPageTranslation

--- general/models.py
+++ general/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from datetime import datetime
 from enum import Enum
+from froala_editor.fields import FroalaField
 
 
 class Language(models.Model):
@@ -20,7 +21,7 @@ class StaticPageTranslation(models.Model):
 	StaticPage = models.ForeignKey(StaticPage, blank=True, null=True, on_delete=models.CASCADE)
 	Language = models.ForeignKey(Language, on_delete=models.DO_NOTHING)
 	Name = models.CharField(default='', max_length=1024)
-	Content = models.TextField(blank=True)
+	Content = FroalaField(blank=True)
 	# Todo: Make single field (Listed/Unlisted/Private)
 	Listed = models.BooleanField(default=False)
 	Private = models.BooleanField(default=True)
