b0c717f4acdc88c6dbd21d4a5babd40e25228bd5
TFotakis <fotakistzanis@gmail.com>
2019-08-15 02:37:07 +0300

    Removed SummernoteAdmin from general admin.py and replaced it with GeneralAdmin to include js.cookie to enable Froala to get csrftoken for all requests

--- general/admin.py
+++ general/admin.py
@@ -1,17 +1,23 @@
 from django.contrib import admin
-from django_summernote.admin import SummernoteModelAdmin
+# from django_summernote.admin import SummernoteModelAdmin
 
 from .models import Language, StaticPage, StaticPageTranslation, NewsletterUser, Ad, AdTranslation
 
 
 # Apply summernote to all TextField in model.
-class SummernoteAdmin(SummernoteModelAdmin):
-	summernote_fields = '__all__'
+# class SummernoteAdmin(SummernoteModelAdmin):
+# 	summernote_fields = '__all__'
+
+class GeneralAdmin(admin.ModelAdmin):
+	class Media:
+		js = (
+			'general/js/js.cookie.min.js',
+		)
 
 
 admin.site.register(Language)
-admin.site.register(StaticPage, SummernoteAdmin)
-admin.site.register(StaticPageTranslation, SummernoteAdmin)
-admin.site.register(NewsletterUser, SummernoteAdmin)
-admin.site.register(Ad, SummernoteAdmin)
-admin.site.register(AdTranslation, SummernoteAdmin)
+admin.site.register(StaticPage)
+admin.site.register(StaticPageTranslation, GeneralAdmin)
+admin.site.register(NewsletterUser)
+admin.site.register(Ad)
+admin.site.register(AdTranslation)
