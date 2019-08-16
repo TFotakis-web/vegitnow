8756a6d695146f15c50d6d4c7c4159b8ac21280f
TFotakis <fotakistzanis@gmail.com>
2019-08-17 00:19:04 +0300

    Adapted new RecipeCard to RecommendedRecipes component & Made Recipes Title clickable

--- frontend/src/components/Recipes/RecommendedRecipes.vue
+++ frontend/src/components/Recipes/RecommendedRecipes.vue
@@ -23,7 +23,11 @@
 				<span aria-hidden="true">></span>
 				<span class="sr-only">Next</span>
 			</a>
-			<h1 class="text-center fgGreen0" style="position: relative;top: -0.5em;margin-bottom: -1.2em;z-index: 1;">{{ $t('Recipes') }}</h1>
+			<h1 class="text-center" style="position: relative;top: -0.5em;margin-bottom: -1.2em;z-index: 1;">
+				<router-link :to="{ name: 'RecipesList' }" class="fgGreen0 bg-transparent">
+					{{ $t('Recipes') }}
+				</router-link>
+			</h1>
 			<div class="bgGreen4 py-5" style="clip-path: polygon(5% 0%, 98% 0%, 97% 20%, 98% 70%, 96% 90%, 97.5% 95%, 98% 100%, 2% 100%, 3% 95%, 2% 80%, 2% 70%, 3% 20%, 2% 0%);">
 				<div id="recipeCarouselTripleCard" class="carousel slide" data-ride="carousel">
 					<div class="carousel-inner">
@@ -51,7 +55,7 @@
 			</div>
 		</div>
 
-		<div id="RecipesDualCard" class="d-none d-lg-block d-xl-none" style="position: relative;">
+		<div id="RecipesDualCard" class="d-none d-md-block d-xl-none" style="position: relative;">
 			<a class="btn btn-circle bgGreen2 text-white" href="#recipeCarouselDualCard" role="button" data-slide="prev" style="
 				   font-size: 1.5rem;
 				   font-family: 'AC-ScriptCondenced_unicode', sans-serif;
@@ -74,7 +78,12 @@
 				<span aria-hidden="true">></span>
 				<span class="sr-only">Next</span>
 			</a>
-			<h1 class="text-center fgGreen0" style="position: relative;top: -0.5em;margin-bottom: -1.2em;z-index: 1;">{{ $t('Recipes') }}</h1>
+
+			<h1 class="text-center" style="position: relative;top: -0.5em;margin-bottom: -1.2em;z-index: 1;">
+				<router-link :to="{ name: 'RecipesList' }" class="fgGreen0 bg-transparent">
+					{{ $t('Recipes') }}
+				</router-link>
+			</h1>
 			<div class="bgGreen4 py-5" style="clip-path: polygon(5% 0%, 98% 0%, 97% 20%, 98% 70%, 96% 90%, 97.5% 95%, 98% 100%, 2% 100%, 3% 95%, 2% 80%, 2% 70%, 3% 20%, 2% 0%);">
 				<div id="recipeCarouselDualCard" class="carousel slide" data-ride="carousel">
 					<div class="carousel-inner">
@@ -102,7 +111,7 @@
 			</div>
 		</div>
 
-		<div id="RecipesSingleCard" class="d-lg-none" style="position: relative;">
+		<div id="RecipesSingleCard" class="d-md-none" style="position: relative;">
 			<a class="btn btn-circle bgGreen2 text-white" href="#recipeCarouselSingleCard" role="button" data-slide="prev" style="
 				   font-size: 1.5rem;
 				   font-family: 'AC-ScriptCondenced_unicode', sans-serif;
@@ -125,7 +134,11 @@
 				<span aria-hidden="true">></span>
 				<span class="sr-only">Next</span>
 			</a>
-			<h1 class="text-center fgGreen0" style="position: relative;top: -0.5em;margin-bottom: -1.2em;z-index: 1;">{{ $t('Recipes') }}</h1>
+			<h1 class="text-center" style="position: relative;top: -0.5em;margin-bottom: -1.2em;z-index: 1;">
+				<router-link :to="{ name: 'RecipesList' }" class="fgGreen0 bg-transparent">
+					{{ $t('Recipes') }}
+				</router-link>
+			</h1>
 			<div class="bgGreen4 py-5" style="clip-path: polygon(5% 0%, 98% 0%, 97% 20%, 98% 70%, 96% 90%, 97.5% 95%, 98% 100%, 2% 100%, 3% 95%, 2% 80%, 2% 70%, 3% 20%, 2% 0%);">
 				<div id="recipeCarouselSingleCard" class="carousel slide" data-ride="carousel">
 					<div class="carousel-inner">
@@ -186,7 +199,9 @@
 		methods: {
 			getArticles: function () {
 				this.$http.get('/api/article/?locale=' + this.$cookie.get('locale') + '&type=1&' + this.location)
-					.then(response => { this.recipeList = response.data; })
+					.then(response => {
+						this.recipeList = response.data;
+					})
 					.catch(this.$root.notifyAction.error);
 			},
 			getAds: function () {
@@ -238,5 +253,7 @@
 </script>
 
 <style scoped>
-
+	#recommendedRecipes {
+		--card-title-shadow: var(--v-green4);
+	}
 </style>
