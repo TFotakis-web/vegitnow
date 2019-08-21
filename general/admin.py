eb307047f495ca8b01432a74dd382c8388fe3648
TFotakis <fotakistzanis@gmail.com>
2019-08-21 17:33:01 +0300

    Restored SummernoteAdmin on general app's admin registration

--- general/admin.py
+++ general/admin.py
@@ -1,23 +1,17 @@
 from django.contrib import admin
-# from django_summernote.admin import SummernoteModelAdmin
+from django_summernote.admin import SummernoteModelAdmin
 
 from .models import Language, StaticPage, StaticPageTranslation, NewsletterUser, Ad, AdTranslation
 
 
 # Apply summernote to all TextField in model.
-# class SummernoteAdmin(SummernoteModelAdmin):
-# 	summernote_fields = '__all__'
-
-class GeneralAdmin(admin.ModelAdmin):
-	class Media:
-		js = (
-			'general/js/js.cookie.min.js',
-		)
+class SummernoteAdmin(SummernoteModelAdmin):
+	summernote_fields = '__all__'
 
 
 admin.site.register(Language)
 admin.site.register(StaticPage)
-admin.site.register(StaticPageTranslation, GeneralAdmin)
+admin.site.register(StaticPageTranslation, SummernoteAdmin)
 admin.site.register(NewsletterUser)
 admin.site.register(Ad)
 admin.site.register(AdTranslation)
