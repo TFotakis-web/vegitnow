62b7a907068d2da9c64fba3a1b2b1117575582af
TFotakis <fotakistzanis@gmail.com>
2019-08-21 17:31:52 +0300

    Restored TextField on StaticPageTranslation model

--- general/models.py
+++ general/models.py
@@ -1,7 +1,6 @@
 from django.db import models
 from datetime import datetime
 from enum import Enum
-from froala_editor.fields import FroalaField
 
 
 class Language(models.Model):
@@ -21,7 +20,7 @@ class StaticPageTranslation(models.Model):
 	StaticPage = models.ForeignKey(StaticPage, blank=True, null=True, on_delete=models.CASCADE)
 	Language = models.ForeignKey(Language, on_delete=models.DO_NOTHING)
 	Name = models.CharField(default='', max_length=1024)
-	Content = FroalaField(blank=True)
+	Content = models.TextField(blank=True)
 	# Todo: Make single field (Listed/Unlisted/Private)
 	Listed = models.BooleanField(default=False)
 	Private = models.BooleanField(default=True)
