b16a4b46e5b95bef7c59e393b079b389ac2ce88a
TFotakis <fotakistzanis@gmail.com>
2019-08-22 16:31:29 +0300

    Implemented renamed admin links on Administration component

--- frontend/src/components/Admin/Administration.vue
+++ frontend/src/components/Admin/Administration.vue
@@ -3,22 +3,22 @@
 		<div class="container h-100">
 			<ul class="nav nav-tabs mb-3">
 				<li class="nav-item">
-					<router-link :to="{ name: 'CreateArticle' }" class="nav-link">{{ $t('Create New') }}</router-link>
+					<router-link :to="{ name: 'AdminCreateArticle' }" class="nav-link">{{ $t('Create New') }}</router-link>
 				</li>
 				<li class="nav-item">
-					<router-link :to="{ name: 'ArticleList' }" class="nav-link">{{ $t('Articles') }}</router-link>
+					<router-link :to="{ name: 'AdminArticlesList' }" class="nav-link">{{ $t('Articles') }}</router-link>
 				</li>
 				<li class="nav-item">
-					<router-link :to="{ name: 'RecipeList' }" class="nav-link">{{ $t('Recipes') }}</router-link>
+					<router-link :to="{ name: 'AdminRecipesList' }" class="nav-link">{{ $t('Recipes') }}</router-link>
 				</li>
 				<li class="nav-item">
-					<router-link :to="{ name: 'IngredientList' }" class="nav-link">{{ $t('Ingredients') }}</router-link>
+					<router-link :to="{ name: 'AdminIngredientsList' }" class="nav-link">{{ $t('Ingredients') }}</router-link>
 				</li>
 				<li class="nav-item">
-					<router-link :to="{ name: 'StaticPages' }" class="nav-link">{{ $t('Static Pages') }}</router-link>
+					<router-link :to="{ name: 'AdminStaticPages' }" class="nav-link">{{ $t('Static Pages') }}</router-link>
 				</li>
 				<li class="nav-item">
-					<router-link :to="{ name: 'Ads' }" class="nav-link">{{ $t('Ads') }}</router-link>
+					<router-link :to="{ name: 'AdminAds' }" class="nav-link">{{ $t('Ads') }}</router-link>
 				</li>
 			</ul>
 			<router-view/>
