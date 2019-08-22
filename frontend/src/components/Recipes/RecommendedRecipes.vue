eac91993f1b67fb394dbdfa7c07a12c4b29e1116
TFotakis <fotakistzanis@gmail.com>
2019-08-22 22:11:53 +0300

    Implemented customizable title and ingredientsIncluded on RecommendedRecipes component

--- frontend/src/components/Recipes/RecommendedRecipes.vue
+++ frontend/src/components/Recipes/RecommendedRecipes.vue
@@ -24,8 +24,8 @@
 				<span class="sr-only">Next</span>
 			</a>
 			<h1 class="text-center" style="position: relative;top: -0.5em;margin-bottom: -1.2em;z-index: 1;">
-				<router-link :to="{ name: 'RecipesList' }" class="fgGreen0 bg-transparent">
-					{{ $t('Recipes') }}
+				<router-link :to="{ name: 'RecipesList', query: { ingredients: ingredientsIncluded.join() } }" class="fgGreen0 bg-transparent">
+					{{ title }}
 				</router-link>
 			</h1>
 			<div class="bgGreen4 py-5" style="clip-path: polygon(5% 0%, 98% 0%, 97% 20%, 98% 70%, 96% 90%, 97.5% 95%, 98% 100%, 2% 100%, 3% 95%, 2% 80%, 2% 70%, 3% 20%, 2% 0%);">
@@ -35,15 +35,15 @@
 							<div class="container">
 								<div class="row">
 									<template v-if="noAdCards">
-										<RecipeCard v-for="(recipe, index2) in recipesPart" :key="'tripleRecipeCard' + recipe.id" :recipe="recipe" :class="{ 'v-border-left v-border-right': index2 === 1}"/>
+										<RecipeCard v-for="(recipe, index2) in recipesPart" :key="'tripleRecipeCard' + recipe.id" :recipe="recipe" :class="{ 'v-border-left': index2 > 0}"/>
 									</template>
 									<template v-else>
 										<template v-for="(recipe, index2) in recipesPart">
 											<template v-if="recipe.hasOwnProperty('article')">
-												<RecipeCard :recipe="recipe['article']" :key="'triple-recipe' + recipe['article'].id" :class="{ 'v-border-left v-border-right': index2 === 1}"/>
+												<RecipeCard :recipe="recipe['article']" :key="'triple-recipe' + recipe['article'].id" :class="{ 'v-border-left': index2 > 0}"/>
 											</template>
 											<template v-else>
-												<AdCard :ad="recipe['ad']" listType="recipes" :key="'triple-ad' + recipe['ad'].id + '-' + index" :class="{ 'v-border-left v-border-right': index2 === 1}"/>
+												<AdCard :ad="recipe['ad']" listType="recipes" :key="'triple-ad' + recipe['ad'].id + '-' + index" :class="{ 'v-border-left': index2 > 0}"/>
 											</template>
 										</template>
 									</template>
@@ -81,7 +81,7 @@
 
 			<h1 class="text-center" style="position: relative;top: -0.5em;margin-bottom: -1.2em;z-index: 1;">
 				<router-link :to="{ name: 'RecipesList' }" class="fgGreen0 bg-transparent">
-					{{ $t('Recipes') }}
+					{{ title }}
 				</router-link>
 			</h1>
 			<div class="bgGreen4 py-5" style="clip-path: polygon(5% 0%, 98% 0%, 97% 20%, 98% 70%, 96% 90%, 97.5% 95%, 98% 100%, 2% 100%, 3% 95%, 2% 80%, 2% 70%, 3% 20%, 2% 0%);">
@@ -136,7 +136,7 @@
 			</a>
 			<h1 class="text-center" style="position: relative;top: -0.5em;margin-bottom: -1.2em;z-index: 1;">
 				<router-link :to="{ name: 'RecipesList' }" class="fgGreen0 bg-transparent">
-					{{ $t('Recipes') }}
+					{{ title }}
 				</router-link>
 			</h1>
 			<div class="bgGreen4 py-5" style="clip-path: polygon(5% 0%, 98% 0%, 97% 20%, 98% 70%, 96% 90%, 97.5% 95%, 98% 100%, 2% 100%, 3% 95%, 2% 80%, 2% 70%, 3% 20%, 2% 0%);">
@@ -182,9 +182,28 @@
 			AdCard
 		},
 		props: {
-			currentRecipeId: String,
-			location: String,
-			noAdCards: Boolean
+			currentRecipeId: {
+				type: String,
+				default: ''
+			},
+			location: {
+				type: String,
+				default: ''
+			},
+			noAdCards: {
+				type: Boolean,
+				default: false
+			},
+			ingredientsIncluded: {
+				type: Array,
+				default: () => []
+			},
+			title: {
+				type: String,
+				default: function () {
+					return this.$root.$t('Recipes');
+				}
+			}
 		},
 		data: function () {
 			return {
@@ -198,7 +217,12 @@
 		},
 		methods: {
 			getArticles: function () {
-				this.$http.get('/api/article/?locale=' + this.$cookie.get('locale') + '&type=1&' + this.location)
+				let requestUrl = '/api/article/?locale=' + this.$cookie.get('locale') + '&type=1';
+				if (this.ingredientsIncluded.length)
+					requestUrl += '&ingredients=' + this.ingredientsIncluded.join();
+				if (this.location)
+					requestUrl += '&' + this.location;
+				this.$http.get(requestUrl)
 					.then(response => {
 						this.recipeList = response.data;
 					})
